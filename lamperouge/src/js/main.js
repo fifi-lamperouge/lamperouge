4d802543a925eb3a0a53a5ede9bd94986c55fe22
fifi-lamperouge <68129876+fifi-lamperouge@users.noreply.github.com>
2020-09-26 01:34:56 +0100

    Add files via upload

---
+++ lamperouge/src/js/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+	$('#open_modal').click(function(){
+		$('#modal_to_open').css(
+		{
+			'display':'block'
+		}
+		);
+
+
+	});
+
+$('#close_modal').click(function(){
+		$('#modal_to_open').css(
+		{
+			'display':'none'
+		}
+		);
+
+
+	});
+});
+
+
